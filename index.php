0c82d21bc551c3a87c9af17ce72c43cb7d92d370
Enjelina <enjelina578@gmail.com>
2022-10-24 12:59:31 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Enjelina";
+    $umur = "20";
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Peppi";
+    $umurAdik = 15;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+?>
